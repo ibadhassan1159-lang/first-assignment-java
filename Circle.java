4e0cfc80ae173000b4e1ca38ceb6bbe761d6f3c3
ibadhassan1159-lang <ibad.hassan1159@gmail.com>
2026-01-11 21:14:43 +0500

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Circle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circle: ");
+        double radius = sc.nextDouble();
+
+        double circumference = 2 * Math.PI * radius;
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Circumference of the circle: " + circumference);
+        System.out.println("Area of the circle: " + area);
+
+        sc.close();
+    }
+}
+
+    
+

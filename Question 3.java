2a8a5a7d92b6c96cbbe58fb71de89f69aac976ca
ibadhassan1159-lang <ibad.hassan1159@gmail.com>
2026-01-11 21:18:41 +0500

    Add files via upload

---
+++ Question 3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DiscountPrice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter original selling price: ");
+        double originalPrice = sc.nextDouble();
+
+        System.out.print("Enter discount percentage: ");
+        double discountPercent = sc.nextDouble();
+
+        double discountAmount = (discountPercent / 100) * originalPrice;
+        double discountedPrice = originalPrice - discountAmount;
+
+        System.out.println("Discounted Selling Price: " + discountedPrice);
+
+        sc.close();
+    }
+}
